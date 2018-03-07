8364abe7b7cfb1f8e4797e74ca4ea8b8687d4a0c
Kevin J Glasow <kevin.glasow@gmail.com>
2018-03-07 13:47:25 -0500

    updated js to have images pausable

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,7 +4,6 @@ var animals = ["Cat", "Dog", "Hamster", "Penguin", "Owl", "Capybara", "Panda", "
 
 // Create ajax request to giphy
 function displayGIF() {
-  // Giphy API Key = "lTJj9ZkaK1SV3zfcpyVEhc9Okz6T5pxC"
   var animal = $(this).attr("data-name");
   var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&limit=10&api_key=lTJj9ZkaK1SV3zfcpyVEhc9Okz6T5pxC"
 
@@ -13,21 +12,25 @@ function displayGIF() {
     method: "GET"
     }).then(function(response) {
     console.log(response);
-	//Creating a loop to go through the response and create an img tag with each URL
+	
+    // Emptying out previous images 
+	$("#image-view").empty();
+
+	// Creating a loop to go through the response and create an img tag with each URL
 	
 	for (var j = 0; j < response.data.length; j++){
-	    var imageURL = response.data[j].images.fixed_height.url;
-	    console.log(imageURL);
-	    console.log(j);
-		$("#image-view").append("<img src=" + imageURL + ">")
+	    var stillURL = response.data[j].images.fixed_height_still.url;
+	    var animatedURL = response.data[j].images.fixed_height.url;
+
+	// Setting the "still" version of the image first
+		
+		$("#image-view").append("<img src=" + stillURL + " data-still=" + stillURL + " data-animated=" + 
+			animatedURL +">")
 		}
-    });
+	});
   }	
 
 // funcation to create the initial buttons at the top of the document
-// TODO: make buttons actually work
-// TODO: make certain that buttons load with the document
-
 function displayButtons() {
 	$("#buttons-view").empty();
 	for (var i = 0; i < animals.length; i++) {
@@ -39,7 +42,7 @@ function displayButtons() {
 	};
 }
 
-// function to trigged when adding a button, adds user text to Animals array and reloads buttons
+// function to be trigged when adding a button, adds user text to Animals array and reloads buttons
 
 $("#add-image").on("click", function(event) {
 	event.preventDefault();
@@ -50,6 +53,14 @@ $("#add-image").on("click", function(event) {
 
 displayButtons();
 $(document).on("click", ".animal-btn", displayGIF);
+$("#image-view").on("click", "img", function() {
+	var animatedURL = $(this).attr("data-animated");
+	var stillURL = $(this).attr("data-still");
+	if ($(this).attr("data-still") === $(this).attr("src"))
+		$(this).attr("src", animatedURL);
+	else $(this).attr("src", stillURL);
+
+	});
 
 
 
